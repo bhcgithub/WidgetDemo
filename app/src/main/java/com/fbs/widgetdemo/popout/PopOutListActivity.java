f6fc8b57ecf4ba5e0a18b7985a4690d7486983ac
baihaichao <baihaichao@5i5j.com>
2019-09-09 17:36:08 +0800

    Initial commit

---
+++ app/src/main/java/com/fbs/widgetdemo/popout/PopOutListActivity.java
@@ -0,0 +1,19 @@
+package com.fbs.widgetdemo.popout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fbs.widgetdemo.R;
+
+/**
+ * 弹框样式列表
+ */
+public class PopOutListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_out_list);
+    }
+}
